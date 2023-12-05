362c1135b2b9356d2bc38a09f6fb3d4586c8dd07
Lucas Leonette da Cruz <88354824+Leo3002@users.noreply.github.com>
2023-12-05 18:10:48 -0300

    Add files via upload

--- Backend/Schemas/Schema_Lembretes.sql
+++ Backend/Schemas/Schema_Lembretes.sql
@@ -1,28 +1,13 @@
 Create database IF NOT EXISTS Lembretes;
-
+drop database Lembretes;
 use Lembretes;
 
 create table IF NOT EXISTS Lembretes( 
 id_Lembretes int primary key not null auto_increment,
 Nome_Lembretes Varchar(50),
 Data_Lembretes date,
-Id_Prioridade int,
-Concluido boolean,
-CONSTRAINT FK_Prioridade FOREIGN KEY (Id_Prioridade)
-    REFERENCES Prioridade(Id_Prioridade)
+Prioridade varchar(13),
+Concluido boolean
 );
-
-create table IF NOT EXISTS Prioridade(
-Id_Prioridade int primary key not null auto_increment,
-Prioridade varchar(20)
-);
-
-insert into Prioridade(Prioridade) values ("Crucial");
-insert into Prioridade(Prioridade) values ("Significativa");
-insert into Prioridade(Prioridade) values ("Moderada");
-insert into Prioridade(Prioridade) values ("Pouca");
-insert into Prioridade(Prioridade) values ("Baixa");
 select * from lembretes;
 select * from Prioridade;
-
-DELETE FROM Prioridade WHERE Id_Prioridade = 5;
